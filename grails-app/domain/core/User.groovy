0ba0e18f83c9e8eb53ff191650b7d14868c62d6f
FelipeAyres <felipe.ayres1993@hotmail.com>
2016-11-12 15:34:10 -0200

    Validação comparação senha

--- grails-app/domain/core/User.groovy
+++ grails-app/domain/core/User.groovy
@@ -7,6 +7,8 @@ class User {
     String password
     String passwordConfirmation
 
+    //static transients = ['passwordConfirmation']
+
     static constraints = {
         name blank: false, nullable: false, minSize: 2
         email email: true, blank: false, nullable: false, unique: true
