114e347ab1a07f449e6c13605d932937186be4cb
Welington Monteiro <welington.rmonteiro@gmail.com>
2016-11-16 12:40:31 -0200

    incluido campo fotos em clientes

--- grails-app/domain/core/Client.groovy
+++ grails-app/domain/core/Client.groovy
@@ -2,8 +2,10 @@ package core
 
 class Client {
     String phone
+    String photo
 
     static constraints = {
         phone blank: false, nullable: false, matches: "\\(\\d{2}\\)\\s\\d{4}-?\\d{4}"
+        photo blank: false, nullable: false
     }
 }
