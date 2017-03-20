3ba94f3690c50ffec5b77f7cde6a0e8c754d8de6
FelipeAyres <felipe.ayres1993@hotmail.com>
2017-03-20 12:19:34 -0300

    Ajuste bug lat e lng editar empresa

--- grails-app/controllers/core/CompanyController.groovy
+++ grails-app/controllers/core/CompanyController.groovy
@@ -231,11 +231,13 @@ class CompanyController {
             address.neighborhood = params.neighborhood
             address.city = params.city
             address.state = params.states
+            address.latitude = params.latitude ? params.latitude.toFloat() : 0f
+            address.longitude = params.longitude ? params.longitude.toFloat() : 0f
             company.companyName = params.companyName
             company.identificationNumber = params.identificationNumber
             company.tradingName = params.tradingName
-//            company.segment = params.segment
-//            company.typeOfCompany = params.typeOfCompany
+            //company.segment = params.segment
+            //company.typeOfCompany = params.typeOfCompany
             company.phone = params.phone
             company.site = params.site
             address.validate()
